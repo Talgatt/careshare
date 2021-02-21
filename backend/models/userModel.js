b05aac054e4e1aac492e644fe2b40f5aa2cff997
Talgatt <talgat001@gmail.com>
2021-02-21 05:44:36 -0800

    merge with frontend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String },
+    firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     image: { type: String },
